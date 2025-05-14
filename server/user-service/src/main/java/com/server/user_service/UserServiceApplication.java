8bfe528196031e5835a84d3c8ef3929cb7ea1b6f
dinhwe2612 <manh5dinh29@gmail.com>
2025-05-14 17:31:15 +0700

    Add user-service module

--- server/user-service/src/main/java/com/server/user_service/UserServiceApplication.java
+++ server/user-service/src/main/java/com/server/user_service/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.image_service.image_service;
+package com.server.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ImageServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImageServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
