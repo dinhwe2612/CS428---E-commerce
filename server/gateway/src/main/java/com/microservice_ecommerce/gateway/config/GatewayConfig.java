b36a086155cc3073e0f23bb8eaf7b9c561e45276
nguyen2192004 <phamgianguyen.qn@gmail.com>
2025-05-14 14:28:54 +0700

    properties

--- server/gateway/src/main/java/com/microservice_ecommerce/gateway/config/GatewayConfig.java
+++ server/gateway/src/main/java/com/microservice_ecommerce/gateway/config/GatewayConfig.java
@@ -2,6 +2,10 @@ package com.microservice_ecommerce.gateway.config;
 
 import com.microservice_ecommerce.gateway.filter.AuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< Updated upstream
+=======
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+>>>>>>> Stashed changes
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -16,9 +20,10 @@ import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 @Configuration
 public class GatewayConfig {
 
-    @Autowired
-    private AuthenticationFilter authFilter;
+        @Autowired
+        private AuthenticationFilter authFilter;
 
+<<<<<<< Updated upstream
     @Bean
     public RouteLocator manualRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -36,3 +41,16 @@ public class GatewayConfig {
         return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
     }
 } 
+=======
+        @Bean
+        public RouteLocator manualRoutes(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("auth_service_all", r -> r.path("/api/v1/auth/**")
+                                                .uri("lb://auth-service"))
+                                .route("image_service_all", r -> r.path("/api/images/**")
+                                                .uri("lb://image-service"))
+                                .build();
+        }
+
+}
+>>>>>>> Stashed changes
