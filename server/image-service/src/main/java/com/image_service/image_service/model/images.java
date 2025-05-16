d2319c588ea42a179e5d9a5b2280d54c69aa61ee
nguyen2192004 <phamgianguyen.qn@gmail.com>
2025-05-16 12:38:51 +0700

    lombok

--- server/image-service/src/main/java/com/image_service/image_service/model/images.java
+++ server/image-service/src/main/java/com/image_service/image_service/model/images.java
@@ -4,14 +4,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class images {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String url;
-  private String publicId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String url;
+    private String publicId;
+
+  
 }
