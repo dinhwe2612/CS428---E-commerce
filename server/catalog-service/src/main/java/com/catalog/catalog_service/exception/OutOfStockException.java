3b0da0db1a94815e68cef1707d0db4ed6000db07
nguyen2192004 <phamgianguyen.qn@gmail.com>
2025-05-22 15:26:48 +0700

    add exception to distributed events

---
+++ server/catalog-service/src/main/java/com/catalog/catalog_service/exception/OutOfStockException.java
@@ -0,0 +1,10 @@
+
+
+package com.catalog.catalog_service.exception;
+
+public class OutOfStockException extends RuntimeException {
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
