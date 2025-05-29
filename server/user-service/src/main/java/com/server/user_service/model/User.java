a36d496054529e57641267f9403c7078bd34bb04
nguyen2192004 <phamgianguyen.qn@gmail.com>
2025-05-29 16:11:58 +0700

    fix the mismatch id between auth service and user service

--- server/user-service/src/main/java/com/server/user_service/model/User.java
+++ server/user-service/src/main/java/com/server/user_service/model/User.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -25,7 +23,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
