e0989d86258a63aa165ab196bf5492da5ccd5014
pt010104 <hisokaxxzk@gmail.com>
2025-06-16 21:36:09 +0700

    Test cart

--- server/cart-service/src/main/java/com/microservice_ecommerce/cart/application/service/CartService.java
+++ server/cart-service/src/main/java/com/microservice_ecommerce/cart/application/service/CartService.java
@@ -5,6 +5,7 @@ import com.microservice_ecommerce.cart.application.dto.CartItemDTO;
 
 import java.util.UUID;
 public interface CartService {
+    //Test cart
     CartDTO addItemToCart(CartItemDTO itemDTO);
     CartDTO removeItemFromCart(UUID itemId);
     CartDTO getCart();
