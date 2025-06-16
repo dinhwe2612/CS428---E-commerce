6bffb0f7373f3388707e06ee3967dcb68b27d633
pt010104 <hisokaxxzk@gmail.com>
2025-06-16 15:50:51 +0700

    Tets3

--- server/cart-service/src/main/java/com/microservice_ecommerce/cart/application/service/CartService.java
+++ server/cart-service/src/main/java/com/microservice_ecommerce/cart/application/service/CartService.java
@@ -4,7 +4,7 @@ import com.microservice_ecommerce.cart.application.dto.CartDTO;
 import com.microservice_ecommerce.cart.application.dto.CartItemDTO;
 
 import java.util.UUID;
-//test 2
+//test 3
 public interface CartService {
     CartDTO addItemToCart(CartItemDTO itemDTO);
     CartDTO removeItemFromCart(UUID itemId);
