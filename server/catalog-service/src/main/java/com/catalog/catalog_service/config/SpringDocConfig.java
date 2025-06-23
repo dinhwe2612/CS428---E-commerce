a3569d51de4863c26a5df9033c03552eb785c805
pt010104 <hisokaxxzk@gmail.com>
2025-06-23 16:10:14 +0700

    Fix Swagger to snakecase

---
+++ server/catalog-service/src/main/java/com/catalog/catalog_service/config/SpringDocConfig.java
@@ -0,0 +1,24 @@
+package com.catalog.catalog_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    @Primary
+    public ModelResolver modelResolver() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new ModelResolver(objectMapper);
+    }
+} 
