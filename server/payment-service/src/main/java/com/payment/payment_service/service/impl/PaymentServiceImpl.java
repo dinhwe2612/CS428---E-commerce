1778c9c7f794019483abe77c0b9fb085b01e24f3
nguyen2192004 <phamgianguyen.qn@gmail.com>
2025-08-03 16:11:02 +0700

    Update PaymentServiceImpl.java

--- server/payment-service/src/main/java/com/payment/payment_service/service/impl/PaymentServiceImpl.java
+++ server/payment-service/src/main/java/com/payment/payment_service/service/impl/PaymentServiceImpl.java
@@ -195,7 +195,7 @@ public class PaymentServiceImpl implements PaymentService {
             PaymentData.PaymentDataBuilder paymentDataBuilder = PaymentData.builder()
                 .orderCode(payment.getOrderId())
                 .amount(payment.getAmount().intValue())
-                .description("Guest payment for order #" + payment.getOrderId())
+                .description("payment for order #" + payment.getOrderId())
                 .item(itemData);
             
             if (request.getReturnUrl() != null) {
