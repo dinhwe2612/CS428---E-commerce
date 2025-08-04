ace0a530a2f4c006397cb993376a101e04a24f37
pt010104 <hisokaxxzk@gmail.com>
2025-08-04 11:47:59 +0700

    Very fast

--- server/catalog-service/src/main/java/com/catalog/catalog_service/repository/jpa/ProductRepository.java
+++ server/catalog-service/src/main/java/com/catalog/catalog_service/repository/jpa/ProductRepository.java
@@ -5,12 +5,18 @@ import java.util.List;
 import com.catalog.catalog_service.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    
+    @EntityGraph(attributePaths = {"images"})
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+    
     Page<Product> findAll(Pageable pageable);
 
     Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
