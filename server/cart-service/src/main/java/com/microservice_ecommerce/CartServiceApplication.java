a129bd3c7754476263c3c5e34ab1f41c3d556877
thinh.tran3 <thinh.tran3@mservice.com.vn>
2025-08-23 08:55:20 +0700

    Revert "Add caching mechanism"
    This reverts commit 9061c817aec629defce7bb51f157ca03b4953a37.

--- server/cart-service/src/main/java/com/microservice_ecommerce/CartServiceApplication.java
+++ server/cart-service/src/main/java/com/microservice_ecommerce/CartServiceApplication.java
@@ -3,11 +3,9 @@ package com.microservice_ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAsync(proxyTargetClass = true)
 public class CartServiceApplication {
     public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
